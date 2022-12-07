f1942dab600a0e619c04b880bcc9dd6a104de9c0
BMarci <balogh.marton.21f@szlgbp.hu>
2022-12-07 21:11:46 +0100

    Add project files.

---
+++ Day 1/Day 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main()
+{
+    std::ifstream input("input.txt");
+    unsigned int first3max[3] { 0, 0, 0 };
+    if (input)
+    {
+        std::string crnString;
+        unsigned int crnSum = 0;
+        while (std::getline(input, crnString))
+        {
+            if (crnString.empty())
+            {
+                if (crnSum > first3max[0]) first3max[0] = crnSum;
+                if (first3max[1] < first3max[0]) std::swap(first3max[1], first3max[0]);
+                if (first3max[2] < first3max[1]) std::swap(first3max[2], first3max[1]);
+                crnSum = 0;
+            }
+            else crnSum += std::stoi(crnString);
+        }
+
+        std::cout << "Part One: " << first3max[2] << std::endl;
+        std::cout << "Part Two: " << first3max[0] + first3max[1] + first3max[2] << std::endl;
+    }
+    if (input.is_open()) input.close();
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
